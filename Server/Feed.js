8dd91e2a8d5cea2a90e5a90952a5eb68e3908863
Jacquin Mininger <jacquin.mininger@gmail.com>
2017-03-21 14:43:06 -0400

    add(Feed-based class methods to answer db)

--- Server/Feed.js
+++ Server/Feed.js
@@ -6,6 +6,16 @@ const express = require('express');
 const router = express.router();
 
 //'/api/feed/'
+
+//NEED A ROUTE FOR GETTING OLD STUFF
+//NEED A ROUTE FOR GETTING NEW STUFF
+
+//Route for getting older stuff(when you scroll all the way to the bottom of the feed)
+router.get('/olderItems/:oldestCurrentId', (req, res, next) => {
+	const oldId = req.params.oldestCurrentId ? req.params.oldestCurrentId : 
+	Answer.findAll({where: {}, limit: 10})
+});
+
 router.get('/:userId/:mostRecentId/:leastRecentId/', (req,res,next) => {
 	Answer.findAll({where: 
 		{respondentId: req.params.userId},
