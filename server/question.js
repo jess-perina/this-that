ccdfada206a3da15246b2f681c0c0a380c0f991c
existentialtype <konstantinos.pouliasis@gmail.com>
2017-03-21 16:49:10 -0400

    feat: question creation by user

---
+++ server/question.js
@@ -0,0 +1,17 @@
+const db = require('APP/db');
+const Answer = db.model('answer');
+const Question = db.model('question');
+const Promise = require('bluebird');
+
+module.exports = require('express').Router()
+.post('/:userId', (req, res, next) => {
+  let {title, leftText, rightText, publicBool, respondents} = req.body;
+  Question.create({title, leftText, rightText, public: publicBool})
+  .then((question) =>{
+    return Promise.map(respondents, (respondent) => {
+      return Answer.create({respondent_id: respondent, question_id: question.id})
+    })
+  })
+  .then(() => res.send(200))
+  .catch(err => console.log(err))
+})
