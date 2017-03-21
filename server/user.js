ef20dcfb4e167c57d0f4ae13450d709d61da0985
existentialtype <konstantinos.pouliasis@gmail.com>
2017-03-21 19:28:29 -0400

    feat: seperate route for public question creation under slash user

---
+++ server/user.js
@@ -0,0 +1,25 @@
+const db = require('APP/db')
+const Answer = db.model('answer')
+const Question = db.model('question')
+const Promise = require('bluebird')
+
+module.exports = require('express').Router()
+.post('/:userId/newprivatequestion', (req, res, next) => {
+  let {title, leftText, rightText, publicBool, respondents} = req.body
+  Question.create({title, leftText, rightText, public: publicBool, owner_id: req.params.userId})
+  .then((question) => {
+    return Promise.map(JSON.parse(respondents), (respondent) => {
+      return Answer.create({respondent_id: respondent, question_id: question.id})
+    })
+  })
+  .then(() => res.send(200))
+  .catch(err => console.log(err))
+})
+
+module.exports = require('express').Router()
+.post('/:userId/newpublicquestion', (req, res, next) => {
+  let {title, leftText, rightText} = req.body
+  Question.create({title, leftText, rightText, public: true, owner_id: req.params.userId})
+  .then(() => res.send(200))
+  .catch(err => console.log(err))
+})
