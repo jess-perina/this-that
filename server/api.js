4f5fba80010e5981340027968ef776e74c9bea10
Jessica Perina <jperina@gmail.com>
2017-03-21 20:10:11 -0400

    refactor(feed routes): make name lower case for consistency

--- server/api.js
+++ server/api.js
@@ -6,8 +6,9 @@ const api = module.exports = require('express').Router() // eslint-disable-line 
 api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
-  .use('/feed', require('./Feed'))
+  .use('/feed', require('./feed'))
   .use('/question', require('./question'))
+  .use('/user', require('./user'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
