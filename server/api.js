f6b32fabc30c09e8f8785a6ce689dd0cc776c800
Jessica Perina <jperina@gmail.com>
2017-03-21 20:29:09 -0400

    feat(users routes): add CRUD routes for users

--- server/api.js
+++ server/api.js
@@ -8,7 +8,7 @@ api
   .use('/auth', require('./auth'))
   .use('/feed', require('./feed'))
   .use('/question', require('./question'))
-  .use('/user', require('./user'))
+  .use('/users', require('./users'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
