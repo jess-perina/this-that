e218ee97ea44d6e3feaeeb91cead7473b17bc5df
Jessica Perina <jperina@gmail.com>
2017-03-23 18:22:17 -0400

    chore(camera): install react native image picker

--- android/app/src/main/java/com/thisthat/MainApplication.java
+++ android/app/src/main/java/com/thisthat/MainApplication.java
@@ -3,6 +3,7 @@ package com.thisthat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RCTCameraPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
