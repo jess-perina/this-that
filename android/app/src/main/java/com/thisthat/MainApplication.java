19cc467c83f4838564a650c3800ce388f71539ba
Jessica Perina <jperina@gmail.com>
2017-03-23 17:06:42 -0400

    feat(camera): configure setting for andriod

--- android/app/src/main/java/com/thisthat/MainApplication.java
+++ android/app/src/main/java/com/thisthat/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactNativeI18n(),
             new VectorIconsPackage(),
             new RNDeviceInfo(),
-            new ReactNativeConfigPackage()
+            new ReactNativeConfigPackage(),
+            new RCTCameraPackage()
       );
     }
   };
