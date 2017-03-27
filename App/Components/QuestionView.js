9cdb9e8632831cb3bc9d19debb547caff53d0fae
Jacquin Mininger <jacquin.mininger@gmail.com>
2017-03-27 17:56:57 -0400

    feat: images in feed

--- App/Components/QuestionView.js
+++ App/Components/QuestionView.js
@@ -1,6 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import styles from './Styles/QuestionViewStyle'
+import FitImage from 'react-native-fit-image'
+
+let myStyles = StyleSheet.create({
+  fitImage: {
+    borderRadius: 20
+  },
+  flex: {
+    flex: 1
+  },
+  fitImageWithSize: {
+    height: 100,
+    width: 30
+  },
+  twoColumnsContainer: {
+    flexDirection: 'row',
+    marginTop: 20
+  }
+})
 
 export default class QuestionView extends React.Component {
 
@@ -11,6 +29,24 @@ export default class QuestionView extends React.Component {
           <Text style={styles.boldLabel} >{this.props.text}</Text>
         </View>
         <View style={styles.optionsContainer} >
+          <Image
+            style={{width: 50, height: 50}}
+            source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          />
+          <View style={styles.twoColumnsContainer}>
+            <FitImage
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+              originalWidth={400}
+              originalHeight={400}
+              style={myStyles.flex}
+       />
+            <FitImage
+              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+              originalWidth={400}
+              originalHeight={400}
+              style={myStyles.flex}
+       />
+          </View>
           <Text style={styles.boldLabel} onPress={this.props.onClickLeft} >{this.props.left}</Text>
           <Text style={styles.boldLabel} onPress={this.props.onClickRight} >{this.props.right}</Text>
         </View>
