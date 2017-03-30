dc0a227e924d2fc441330e5a7da498e303aab04f
Jessica Perina <jperina@gmail.com>
2017-03-30 13:10:03 -0400

    style(app): update styles to new colors

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,5 +1,5 @@
 const colors = {
-  background: '#1F0808',
+  background: '#00B297',
   clear: 'rgba(0,0,0,0)',
   facebook: '#3b5998',
   transparent: 'rgba(0,0,0,0)',
@@ -21,7 +21,9 @@ const colors = {
   eggplant: '#251a34',
   border: '#483F53',
   banner: '#5F3E63',
-  text: '#E0D7E5'
+  text: '#E0D7E5',
+  peach: '#FF8959',
+  smolder: '#B22A03'
 }
 
 export default colors
